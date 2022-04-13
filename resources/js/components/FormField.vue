141e9a797c48c24c23d788e555129a4b88c7a4fb
dmason30 <danmason@spendology.com>
2022-04-13 19:02:37 +0100

    Adds Nova 4.x Support

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,30 +1,33 @@
 <template>
-    <default-field :field="field" :errors="errors" :show-help-text="showHelpText">
-        <template slot="field">
-            <vue-tags-input v-model="tag" :tags="tags" @tags-changed="tagsChanged"
-                            :placeholder="field.placeholder"
-                            :autocompleteItems="filteredItems"
-                            :add-on-key="field.addOnKeys"
-                            :separators="field.separators"
-                            :add-from-paste="field.addFromPaste"
-                            :add-on-blur="field.addOnBlur"
-                            :add-only-from-autocomplete="field.addOnlyFromAutocomplete"
-                            :allow-edit-tags="field.allowEditTags"
-                            :autocomplete-always-open="field.autocompleteAlwaysOpen"
-                            :autocomplete-filter-duplicates="field.autocompleteFilterDuplicates"
-                            :autocomplete-min-length="field.autocompleteMinLength"
-                            :avoid-adding-duplicates="field.avoidAddingDuplicates"
-                            :delete-on-backspace="field.deleteOnBackspace"
-                            :disabled="field.disabled"
-                            :max-tags="field.maxTags"
+    <DefaultField :field="field" :errors="errors" :show-help-text="showHelpText">
+        <template #field>
+            <vue-tags-input
+                v-model="tag"
+                :tags="tags"
+                @tags-changed="tagsChanged"
+                :placeholder="field.placeholder"
+                :autocompleteItems="filteredItems"
+                :add-on-key="field.addOnKeys"
+                :separators="field.separators"
+                :add-from-paste="field.addFromPaste"
+                :add-on-blur="field.addOnBlur"
+                :add-only-from-autocomplete="field.addOnlyFromAutocomplete"
+                :allow-edit-tags="field.allowEditTags"
+                :autocomplete-always-open="field.autocompleteAlwaysOpen"
+                :autocomplete-filter-duplicates="field.autocompleteFilterDuplicates"
+                :autocomplete-min-length="field.autocompleteMinLength"
+                :avoid-adding-duplicates="field.avoidAddingDuplicates"
+                :delete-on-backspace="field.deleteOnBackspace"
+                :disabled="field.disabled"
+                :max-tags="field.maxTags"
             />
         </template>
-    </default-field>
+    </DefaultField>
 </template>
 
 <script>
 import { FormField, HandlesValidationErrors } from 'laravel-nova'
-import VueTagsInput from '@johmun/vue-tags-input';
+import VueTagsInput from '@sipec/vue3-tags-input';
 
 export default {
     data() {
--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,6 @@
-Nova.booting((Vue, router, store) => {
-    Vue.component('index-nova-tag-input', require('./components/IndexField'))
-    Vue.component('detail-nova-tag-input', require('./components/DetailField'))
-    Vue.component('form-nova-tag-input', require('./components/FormField'))
+Nova.booting((Vue) => {
+    window.Vue.use = Vue.use;
+    Vue.component('index-nova-tag-input', require('./components/IndexField').default)
+    Vue.component('detail-nova-tag-input', require('./components/DetailField').default)
+    Vue.component('form-nova-tag-input', require('./components/FormField').default)
 })
